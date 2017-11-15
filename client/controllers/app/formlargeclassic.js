8f3d1b235333dac967284ce8405810681b571595
Kent Zeng <Kent.Zeng@flex.com>
2017-11-14 19:21:56 -0800

    [Studio] Add screen "FormLarge-Classic"

---
+++ client/controllers/app/formlargeclassic.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_formlargeclassic', app_formlargeclassic);
+function app_formlargeclassic($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.formlargeclassic', {
+        views: {
+            app: {
+                controller: 'app_formlargeclassic',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.formlargeclassic');
+                }
+            }
+        }
     });
 });
